7a052244009ccfe22bf82153a207a18890c0cf46
Josh Nesbitt <josh@josh-nesbitt.net>
2011-01-14 14:06:36 +0000

    Cleanup.

--- lib/google/url_shortener/errors.rb
+++ lib/google/url_shortener/errors.rb
@@ -1,9 +1,8 @@
 module Google
   module UrlShortener
-    
     class UrlShortenerError < StandardError
       attr_accessor :data
-
+      
       def initialize(data)
         self.data = data
         super
