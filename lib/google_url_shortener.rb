bf3a436414b7ed5622da9b2c9cbfdc2c6b8d6a3f
Josh Nesbitt <josh@josh-nesbitt.net>
2011-01-14 12:39:29 +0000

    Use absolute path for require.

--- lib/google_url_shortener.rb
+++ lib/google_url_shortener.rb
@@ -4,7 +4,9 @@ require 'restclient'
 require 'json'
 # EO TODO
 
+ROOT = File.expand_path(File.dirname(__FILE__) + "/..")
+
 %w{ version
     request
     base
-    url }.each { |f| require("google/url_shortener/#{f}") }
+    url }.each { |f| require("#{ROOT}/lib/google/url_shortener/#{f}") }
